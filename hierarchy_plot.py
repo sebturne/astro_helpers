ebeba8ccdfe771e85fd79eff8612673f97fab5f6
Sebastian Turner <seb.turne@gmail.com>
2023-08-25 16:50:59 +0100

    replacing hierarchy files, still TBC though

--- hierarchy_plot.py
+++ hierarchy_plot.py
@@ -1,4 +1,4 @@
-import eagle_functions as E
+import gama_functions as G
 import numpy as N
 import matplotlib as M
 M.use('Agg')
@@ -7,12 +7,12 @@ import sklearn.metrics as SM
 
 MP.rcParams.update({'figure.figsize':[4,4], 'figure.dpi': 300, 'font.family': 'Times New Roman', 'font.size': 10, 'text.usetex': True})
 
-feat_set = 'mstar_smdust_ssfr_hlr_bt'
-k = [2,3,4,6]
+feat_set = 'mstar_n_ssfr_sig5'
+k = [2,3,6,9]
 
 zscr = N.genfromtxt('Pre/' + feat_set + '_tlz.txt', delimiter = ',')
-lbls = N.genfromtxt('../../Data/Clustering/EAGLE/RefL0100N1504_z_0/Post/Labels/' + feat_set + '_tlz_lbls_k%i.txt' % k[0], delimiter = ',')
-phi  = N.genfromtxt('Post/Clustering/' + feat_set + '_tlz_staco.txt', delimiter = ',')[:,1:]
+lbls = N.genfromtxt('../../Data/Clustering2/' + feat_set + '_tlz_lbls_k%i.txt' % k[0], delimiter = ',')
+phi  = N.genfromtxt('../../Data/Clustering2/' + feat_set + '_tlz_staco.txt', delimiter = ',')[:,1:]
 phi  = phi[phi[:,1] == k[0],:]
 
 best = N.where(phi[:,0] == min(phi[:,0]))[0][0]
@@ -23,27 +23,28 @@ n = [0] * (k[0])
 for j in range(k[0]):
     n[j] = len(clst[clst[:,0] == j,0])
 
+ma = G.FS(zscr[:,1], clst[:,0])
+
 ya = N.arange(0.5-(float(k[0])/2),(float(k[0])/2),1)
-ma = E.FS(zscr[:,2], clst[:,0])
 
 fig, ax = MP.subplots()
 
 for j in range(k[0]):
-    ax.text(0, ya[j], '$%i$' % (n[ma[j]]), ha = 'center', va = 'center', bbox = dict(facecolor = '#ffffff', edgecolor = '#000000', boxstyle = 'round,pad=0.5'))
+    ax.text(0, ya[j], '$%i$' % (n[ma[j]]), ha = 'center', va = 'center', bbox = dict(facecolor = 'w', edgecolor = 'k', boxstyle = 'round,pad=0.5'))
 
 ax.text(0, ya[j] + 0.75, '$k = %i$' % k[0], ha = 'center', va = 'center')
 
 for i in range(1,len(k)):
 
-    lbls = N.genfromtxt('../../Data/Clustering/EAGLE/RefL0100N1504_z_0/Post/Labels/' + feat_set + '_tlz_lbls_k%i.txt' % k[i], delimiter = ',')
-    phi = N.genfromtxt('Post/Clustering/' + feat_set + '_tlz_staco.txt', delimiter = ',')[:,1:]
+    lbls = N.genfromtxt('../../Data/Clustering2/' + feat_set + '_tlz_lbls_k%i.txt' % k[i], delimiter = ',')
+    phi = N.genfromtxt('../../Data/Clustering2/' + feat_set + '_tlz_staco.txt', delimiter = ',')[:,1:]
     phi = phi[phi[:,1] == k[i],:]
     
     best = N.where(phi[:,0] == min(phi[:,0]))[0][0]
     clst = N.hstack((clst,lbls[:,best].reshape(-1,1)))
 
     yb = N.arange(0.5-(float(k[i])/2),(float(k[i])/2),1)
-    mb = E.FS(zscr[:,2], clst[:,i])
+    mb = G.FS(zscr[:,2], clst[:,i])
 
     coma = SM.confusion_matrix(clst[:,i-1],clst[:,i])
     coma = coma[:(k[i-1]), :(k[i])]
@@ -61,17 +62,17 @@ for i in range(1,len(k)):
                 o = ((float(coma[l,j]) / float(sum(coma[l,:])))**(1))
             else:
                 o = ((float(coma[l,j]) / float(sum(coma[:,j])))**(1))
-            ax.arrow(i-1, ya[l], 1, yb[j] - ya[l], fc = '#ff0000', ec = '#ff0000', alpha = o)
+            ax.arrow(i-1, ya[l], 1, yb[j] - ya[l], fc = 'r', ec = 'r', alpha = o)
 
     for j in range(k[i]):
-        ax.text(i, yb[j], '$%i$' % (n[mb[j]]), ha = 'center', va = 'center', bbox = dict(facecolor = '#ffffff', edgecolor = '#000000', boxstyle ='round,pad=0.5'))
+        ax.text(i, yb[j], '$%i$' % (n[mb[j]]), ha = 'center', va = 'center', bbox = dict(facecolor = 'w', edgecolor = 'k', boxstyle ='round,pad=0.5'))
 
     ax.text(i, yb[j] + 0.75, '$k = %i$' % k[i], ha = 'center', va = 'center')
     
     ya = yb
     ma = mb
 
-#ax.plot([0,1,2,3],[0,0.5,0.5,1],'k--')
+ax.plot([0,1,2,3],[0,0.5,0.5,1],'k--')
 
 ax.set(xticks = [], yticks = [], xlim = (-0.5,len(k)-0.5), ylim = (-4,4))
 ax.axis('off')
